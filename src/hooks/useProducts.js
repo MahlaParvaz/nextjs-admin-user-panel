81ce8337c50d9fc0de3640282b98af3a40ef5fd6
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-02-04 10:01:48 +0330

    add: edit and update product

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,6 +2,7 @@ import {
   getProducts,
   addProduct,
   getOneProdcutById,
+  updateProduct,
 } from '@/services/productService';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
@@ -17,9 +18,12 @@ export const useAddProduct = () => {
   return useMutation({ mutationFn: addProduct });
 };
 
+export const useUpdateProduct = () => {
+  return useMutation({ mutationFn: updateProduct });
+};
 export const useGetProductById = (id) =>
   useQuery({
-    queryKey: ['get-product'],
+    queryKey: ['get-product', id],
     queryFn: () => getOneProdcutById(id),
     retry: false,
     refetchOnWindowFocus: true,
